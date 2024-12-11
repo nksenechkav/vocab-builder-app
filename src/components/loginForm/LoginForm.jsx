342d495c5656af7d2ca21a57048e832207337d76
Oksana <n.ksenechka.v@gmail.com>
2024-12-11 13:42:23 +0100

    correct validation in register and login pages

--- src/components/loginForm/LoginForm.jsx
+++ src/components/loginForm/LoginForm.jsx
@@ -1,30 +1,32 @@
 // src/components/LoginForm/LoginForm.jsx
 
+
+import { logIn } from '../../redux/auth/operations';
 import { Formik, Form, Field } from 'formik';
-import { ErrorMessage } from "formik";
-import * as Yup from "yup";
+import { ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 import { useId, useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { logIn } from '../../redux/auth/operations';
 import toast, { Toaster } from 'react-hot-toast';
 import css from './LoginForm.module.scss';
 import { RiEyeOffLine, RiEyeLine } from 'react-icons/ri';
+import { MdError } from 'react-icons/md';
+import { AiFillCheckCircle } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
 
 const FeedbackSchema = Yup.object().shape({
-  email: Yup.string().trim()
-  .matches(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/, 'Invalid email address')
-  .required("Required")
-  .test('is-valid-email', 'Enter a valid email address', function (value) {
-    return /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/.test(value);
-  }),
+  email: Yup.string()
+    .trim()
+    .matches(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/, 'Invalid email address')
+    .required("Required"),
 
-  password: Yup.string().trim()
-  .matches(/^(?=.*[a-zA-Z])(?=.*\d).{5,}$/, 'Password must contain at least one letter, one digit, and be at least 5 characters long')
-  .required("Required")
-  .test('is-valid-password', 'Must be a valid password!', function (value) {
-    return /^(?=.*[a-zA-Z])(?=.*\d).{5,}$/.test(value);
-  })
+  password: Yup.string()
+    .trim()
+    .matches(
+      /^(?=.*[a-zA-Z]{6})(?=.*\d)[a-zA-Z\d]{7}$/, 
+      'Password must contain at least 6 letters, at least 1 digit, and be exactly 7 characters long'
+    )
+    .required("Required"),
 });
 
 export const LoginForm = ({ onLoginSuccess }) => {
@@ -33,10 +35,16 @@ export const LoginForm = ({ onLoginSuccess }) => {
   const passwordFieldId = useId();
 
   const [showPassword, setShowPassword] = useState(false);
+  const [passwordValid, setPasswordValid] = useState(null); // State to track password validity
 
   // Функция для переключения видимости пароля
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
+  };
+
+  const handlePasswordValidation = (value) => {
+    const isValid = /^(?=.*[a-zA-Z]{6})(?=.*\d)[a-zA-Z\d]{7}$/.test(value);
+    setPasswordValid(isValid);
   };
 
   const handleSubmit = (values, actions) => {
@@ -48,6 +56,7 @@ export const LoginForm = ({ onLoginSuccess }) => {
       .then(() => toast.success('Login success. Congratulations!'))
       .catch(() => toast.error('Login failed. Please check your credentials!'));
     actions.resetForm();
+    setPasswordValid(null);
     onLoginSuccess();
   };
   return (
@@ -60,22 +69,40 @@ export const LoginForm = ({ onLoginSuccess }) => {
     <p className={css['form-header']}>Login</p>
     <p className={css['form-text']}>Please enter your login details to continue using our service:</p>
       <div className={css['form-wrapper']}>
-        {/* <label className={css.label} htmlFor={emailFieldId}>Email</label> */}
         <Field className={css.field} type="email" name="email" id={emailFieldId} placeholder="Email" autoComplete="current-email"/>
         <ErrorMessage name="email" component="p" className={css.error} />
       </div>
       <div className={css['form-wrapper']}>
-        {/* <label className={css.label} htmlFor={passwordFieldId}>Password</label> */}
-        <Field className={css.field}  type={showPassword ? 'text' : 'password'} name="password" id={passwordFieldId} placeholder="Password" autoComplete="current-password"/> 
-        <button
-        type="button"
-        onClick={togglePasswordVisibility}
-        className={css['my-icon']}
-      >
-        {showPassword ? <RiEyeLine size={20} /> : <RiEyeOffLine size={20} />}
-         </button>
-        <ErrorMessage name="password" component="p" className={css.error} />
-      </div>
+            <Field
+              className={css.field}
+              type={showPassword ? 'text' : 'password'}
+              name="password"
+              id={passwordFieldId}
+              placeholder="Password"
+              autoComplete="current-password"
+              onKeyUp={(e) => handlePasswordValidation(e.target.value)}
+            />
+            <button
+              type="button"
+              onClick={togglePasswordVisibility}
+              className={css['my-icon']}
+            >
+              {showPassword ? <RiEyeLine size={20} /> : <RiEyeOffLine size={20} />}
+            </button>
+            <div className={css.feedback}>
+              {passwordValid === false && (
+                <p className={css.errorMessage}>
+                  <MdError size={20} style={{ color: 'white', fill: 'red', borderRadius: '50%' }} /> Error password
+                </p>
+              )}
+              {passwordValid === true && (
+                <p className={css.successMessage}>
+                  <AiFillCheckCircle size={20} style={{ color: 'white', fill: 'green', borderRadius: '50%' }} /> Success password
+                </p>
+              )}
+            </div>
+            <ErrorMessage name="password" component="p" className={css.error} />
+          </div>
       <button className={css.btn} type="submit">Login</button>
        <p className={css.registerLink}>
         <Link to="/register" className={css["register-link"]}>
--- src/components/registrationForm/RegistrationForm.jsx
+++ src/components/registrationForm/RegistrationForm.jsx
@@ -9,23 +9,23 @@ import { register } from '../../redux/auth/operations';
 import toast, { Toaster } from 'react-hot-toast';
 import css from './RegistrationForm.module.scss';
 import { RiEyeOffLine, RiEyeLine } from 'react-icons/ri';
+import { MdError } from 'react-icons/md';
+import { AiFillCheckCircle } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
 
-
 const FeedbackSchema = Yup.object().shape({
-  email: Yup.string().trim()
-  .matches(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/, 'Invalid email address')
-  .required("Required")
-  .test('is-valid-email', 'Enter a valid email address', function (value) {
-    return /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/.test(value);
-  }),
+  email: Yup.string()
+    .trim()
+    .matches(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/, 'Invalid email address')
+    .required("Required"),
 
-  password: Yup.string().trim()
-  .matches(/^(?=.*[a-zA-Z])(?=.*\d).{5,}$/, 'Password must contain at least one letter, one digit, and be at least 5 characters long')
-  .required("Required")
-  .test('is-valid-password', 'Must be a valid password!', function (value) {
-    return /^(?=.*[a-zA-Z])(?=.*\d).{5,}$/.test(value);
-  })
+  password: Yup.string()
+    .trim()
+    .matches(
+      /^(?=.*[a-zA-Z]{6})(?=.*\d)[a-zA-Z\d]{7}$/, 
+      'Password must contain at least 6 letters, at least 1 digit, and be exactly 7 characters long'
+    )
+    .required("Required"),
 });
 
 export const RegistrationForm = () => {
@@ -35,64 +35,108 @@ export const RegistrationForm = () => {
   const passwordFieldId = useId();
 
   const [showPassword, setShowPassword] = useState(false);
+  const [passwordValid, setPasswordValid] = useState(null); // State to track password validity
 
-  // Функция для переключения видимости пароля
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
 
+  const handlePasswordValidation = (value) => {
+    const isValid = /^(?=.*[a-zA-Z]{6})(?=.*\d)[a-zA-Z\d]{7}$/.test(value);
+    setPasswordValid(isValid);
+  };
+
   const handleSubmit = (values, actions) => {
-    dispatch(register({
-      name: values.name,
-      email: values.email,
-      password: values.password,
-    }))
+    dispatch(
+      register({
+        name: values.name,
+        email: values.email,
+        password: values.password,
+      })
+    )
       .unwrap()
       .then(() => toast.success('Registration success. Congratulations!'))
       .catch(() => toast.error('Registration failed. Please check your credentials!'));
     actions.resetForm();
+    setPasswordValid(null);
   };
-
 
   return (
     <Formik
-    initialValues={{ name: '', email: '', password: '' }}
-    onSubmit={handleSubmit}
-    validationSchema={FeedbackSchema}
-  >
-    <Form className={css.form} autoComplete="off">
-    <p className={css['form-header']}>Register</p>
-    <p className={css['form-text']}>To start using our services, please fill out the registration form below. All fields are mandatory:</p>
-      <div className={css['form-wrapper']}>
-        {/* <label className={css.label} htmlFor={nameFieldId}>Username</label> */}
-        <Field className={css.field} type="text" name="name" id={nameFieldId} placeholder="Name" autoComplete="current-username"/>
-        <ErrorMessage name="name" component="p" className={css.error} />
-      </div>
-      <div className={css['form-wrapper']}>
-        {/* <label className={css.label} htmlFor={emailFieldId}>Email</label> */}
-        <Field className={css.field} type="email" name="email" id={emailFieldId} placeholder="Email" autoComplete="current-email"/>
-        <ErrorMessage name="email" component="p" className={css.error} />
-      </div>
-      <div className={css['form-wrapper']}>
-        {/* <label className={css.label} htmlFor={passwordFieldId}>Password</label> */}
-        <Field className={css.field}  type={showPassword ? 'text' : 'password'} name="password" id={passwordFieldId} placeholder="Password" autoComplete="current-password"/> 
-        <button
-        type="button"
-        onClick={togglePasswordVisibility}
-        className={css['my-icon']}
-      >
-        {showPassword ? <RiEyeLine size={20} /> : <RiEyeOffLine size={20} />}
-         </button>
-        <ErrorMessage name="password" component="p" className={css.error} />
-      </div>
-      <button className={css.btn} type="submit">Register</button>
-      <p className={css.loginLink}>
-        <Link to="/login" className={css["login-link"]}>
-          Login
-        </Link>
-      </p>
-      <Toaster />
-    </Form>
-  </Formik>
+      initialValues={{ name: '', email: '', password: '' }}
+      onSubmit={handleSubmit}
+      validationSchema={FeedbackSchema}
+    >
+      {({ values }) => (
+        <Form className={css.form} autoComplete="off">
+          <p className={css['form-header']}>Register</p>
+          <p className={css['form-text']}>
+            To start using our services, please fill out the registration form below. All fields are mandatory:
+          </p>
+          <div className={css['form-wrapper']}>
+            <Field
+              className={css.field}
+              type="text"
+              name="name"
+              id={nameFieldId}
+              placeholder="Name"
+              autoComplete="current-username"
+            />
+            <ErrorMessage name="name" component="p" className={css.error} />
+          </div>
+          <div className={css['form-wrapper']}>
+            <Field
+              className={css.field}
+              type="email"
+              name="email"
+              id={emailFieldId}
+              placeholder="Email"
+              autoComplete="current-email"
+            />
+            <ErrorMessage name="email" component="p" className={css.error} />
+          </div>
+          <div className={css['form-wrapper']}>
+            <Field
+              className={css.field}
+              type={showPassword ? 'text' : 'password'}
+              name="password"
+              id={passwordFieldId}
+              placeholder="Password"
+              autoComplete="current-password"
+              onKeyUp={(e) => handlePasswordValidation(e.target.value)}
+            />
+            <button
+              type="button"
+              onClick={togglePasswordVisibility}
+              className={css['my-icon']}
+            >
+              {showPassword ? <RiEyeLine size={20} /> : <RiEyeOffLine size={20} />}
+            </button>
+            <div className={css.feedback}>
+              {passwordValid === false && (
+                <p className={css.errorMessage}>
+                  <MdError size={20} style={{ color: 'white', fill: 'red', borderRadius: '50%' }} /> Error password
+                </p>
+              )}
+              {passwordValid === true && (
+                <p className={css.successMessage}>
+                  <AiFillCheckCircle size={20} style={{ color: 'white', fill: 'green', borderRadius: '50%' }} /> Success password
+                </p>
+              )}
+            </div>
+            <ErrorMessage name="password" component="p" className={css.error} />
+          </div>
+          <button className={css.btn} type="submit">
+            Register
+          </button>
+          <p className={css.loginLink}>
+            <Link to="/login" className={css['login-link']}>
+              Login
+            </Link>
+          </p>
+          <Toaster />
+        </Form>
+      )}
+    </Formik>
   );
 };
