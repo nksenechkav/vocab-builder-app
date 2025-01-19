31f490f3c338be207e4be8d1e006eda9958d1ada
Oksana <n.ksenechka.v@gmail.com>
2025-01-19 13:46:50 +0100

    add fix in words pagination

--- src/components/wordsPagination/WordsPagination.jsx
+++ src/components/wordsPagination/WordsPagination.jsx
@@ -1,7 +1,8 @@
 // src/components/wordsPagination/WordsPagination.jsx
 
-import { useEffect } from 'react';
-import { Button } from '@mui/material';
+// src/components/wordsPagination/WordsPagination.jsx
+
+import { FaAngleLeft, FaAngleRight, FaAngleDoubleLeft, FaAngleDoubleRight } from 'react-icons/fa';
 import css from './WordsPagination.module.scss';
 
 const WordsPagination = ({ fetchData, currentPage, pageSize, totalPages }) => {
@@ -12,31 +13,66 @@ const WordsPagination = ({ fetchData, currentPage, pageSize, totalPages }) => {
     }
   };
 
-  useEffect(() => {
-    // You can add any other side effects if needed here.
-  }, [currentPage, totalPages]);
+  // Render pagination buttons
+  const renderPageButtons = () => {
+    const buttons = [];
+    for (let i = 1; i <= totalPages; i++) {
+      buttons.push(
+        <button
+          key={i}
+          onClick={() => handlePageChange(i)}
+          className={`${css['pagination-button']} ${i === currentPage ? css['active-page'] : ''}`}
+        >
+          {i}
+        </button>
+      );
+    }
+    return buttons;
+  };
 
   return (
     <div className={css['pagination-container']}>
-      <Button
+      {/* First Page Button */}
+      <button
+        onClick={() => handlePageChange(1)}
+        className={`${css['pagination-button']} ${currentPage === 1 ? css['active-arrow'] : ''}`}
+      >
+        <FaAngleDoubleLeft className={css['icon-arrow']} size={16} />
+      </button>
+
+      {/* Previous Button */}
+      <button
         onClick={() => handlePageChange(currentPage - 1)}
         disabled={currentPage === 1}
-        className={css['pagination-button']}
+        className={`${css['pagination-button']} ${currentPage === 1 ? css['disabled'] : ''}`}
       >
-        Previous
-      </Button>
-      <span className={css['pagination-text']}>
-        Page {currentPage} of {totalPages}
-      </span>
-      <Button
+        <FaAngleLeft className={css['icon-arrow']} size={16} />
+      </button>
+
+      {/* Page Number Buttons */}
+      {renderPageButtons()}
+
+      {/* Next Button */}
+      <button
         onClick={() => handlePageChange(currentPage + 1)}
         disabled={currentPage === totalPages}
-        className={css['pagination-button']}
+        className={`${css['pagination-button']} ${currentPage === totalPages ? css['disabled'] : ''}`}
       >
-        Next
-      </Button>
+        <FaAngleRight className={css['icon-arrow']} size={16} />
+      </button>
+
+      {/* Last Page Button */}
+      <button
+        onClick={() => handlePageChange(totalPages)}
+        className={`${css['pagination-button']} ${currentPage === totalPages ? css['active-arrow'] : ''}`}
+      >
+        <FaAngleDoubleRight className={css['icon-arrow']} size={16} />
+      </button>
     </div>
   );
 };
 
 export default WordsPagination;
+
+
+
